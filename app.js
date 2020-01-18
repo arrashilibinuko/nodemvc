0bcf3af536a7d9707bb0bfbe8db786186b198fc6
Arrashi <arrashilibinuko.19@ichat.sp.edu.sg>
2020-01-18 10:58:38 +0800

    updated

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
+var createError = require('http-errors');
 var express = require('express');
-var path = require("path");
-var config = require("./config");
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
-var index = require("./routes/index");
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// app.use('/',function(req,res){
-//     res.send("hello workd");
-//     res.end();
-// });
-app.use('/',index);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/',(req,res))
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-if(!module.parent){
-    app.listen(config.port, function(){
-        console.log(`hello server is running on port  ${config.port}`)
-    })
-}
-module.exports = app
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var home = require('../contollers/home');
-var user = require('../contollers/users');
-
-
-router.get('/',home.index);
-router.get('/user',user.getusers);
-router.get('/user/delete',user.deleteusers)
-
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
